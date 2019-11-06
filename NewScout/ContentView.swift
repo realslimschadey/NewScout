e4afb41cf98ea762a5de3d3e2d1ab8762d6564e9
slimschadey <realslimschadey@gmail.com>
2019-11-06 17:30:52 -0500

    Initial Commit

---
+++ NewScout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewScout
+//
+//  Created by Michael Schade on 11/6/19.
+//  Copyright © 2019 Michael Schade. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
